36da984aa2f50871655c21f1b2ea432626cefc68
PuRakkk <sarakchon11@gmail.com>
2025-06-19 10:17:59 +0700

    Testing Git

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,23 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import helloworld from '@/views/helloworld.vue';
 
 const routerHistory = createWebHistory();
 
 const router = createRouter({
   history: routerHistory,
   routes: [
-    {
-      path: '/helloworld',
-      name: 'helloworld',
-      component: helloworld,
-      meta: { title: "Hello World" },
-    },
+    // {
+    //   path: '/helloworld',
+    //   name: 'helloworld',
+    //   component: helloworld,
+    //   meta: { title: "Hello World" },
+    // },
   ], 
 });
 
 // Global Navigation Guard
 router.beforeEach((to, _from, next) => {
-  document.title = `social-app ${to.meta.title || ''}`;
+  document.title = `Social App ${to.meta.title || ''}`;
   next();
 });
 
